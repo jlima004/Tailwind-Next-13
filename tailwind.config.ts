a4f2c9a9235897f42697db9ee034c767b1f687b1
jlima004 <jlima004@gmail.com>
2023-09-30 20:36:28 -0300

    remove colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,6 @@ const config: Config = {
         app: 'minmax(18rem, 20rem) 1fr',
         profile: 'max-content 1fr min-content',
       },
-
-      colors: {
-        jeff: '#666',
-      },
     },
   },
   plugins: [],
