e35f5a321974842022e164e45f310a62707db265
jlima004 <jlima004@gmail.com>
2023-10-02 21:41:51 -0300

    add: form

---
+++ src/components/Form/FileInput/index.tsx
@@ -0,0 +1,7 @@
+import { Control } from './Control'
+import { ImagePreview } from './ImagePreview'
+import { Root } from './Root'
+import { Trigger } from './Trigger'
+import { FieldList } from './FieldList'
+
+export { Control, ImagePreview, Root, Trigger, FieldList }
